126c581681eb7dc7145a1debc55fe6ddd4f4ce18
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-06 16:29:07 +0900

    adding tests

--- cpp/test/ch03/maze-state-test.cc
+++ cpp/test/ch03/maze-state-test.cc
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 #include "src/ch03/maze-state.h"
 
+using namespace std;
+
 TEST(HelloTest, FirstTest)
 {
     EXPECT_EQ(1, 1);
@@ -9,4 +11,17 @@ TEST(HelloTest, FirstTest)
 TEST(MazeState, Init)
 {
     MazeState state = MazeState(0);
+    // if seed is working
+    EXPECT_EQ(state.character_.x_, 3);
+    EXPECT_EQ(state.character_.y_, 2);
+
+    // sum of points is not 0
+}
+
+TEST(MazeState, LegalActions)
+{
+    MazeState state = MazeState(0);
+    auto actual = state.legal_actions();
+    std::vector<int> expected = {1, 3};
+    EXPECT_EQ(actual, expected);
 }

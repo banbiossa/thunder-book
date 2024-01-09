b477b614d12c7e65540c574ce59da404384fb4f4
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-09 20:18:01 +0900

    add chokudai search

---
+++ cpp/src/ch03/chokudai_search.cc
@@ -0,0 +1,66 @@
+#include <queue>
+#include "chokudai_search.h"
+
+int chokudai_search_action(
+    const MazeState &state,
+    const int beam_width,
+    const int beam_depth,
+    const int beam_number)
+{
+    // init
+    auto beam = std::vector<std::priority_queue<MazeState>>(beam_depth + 1);
+    for (int t = 0; t < beam_depth + 1; t++)
+    {
+        beam[t] = std::priority_queue<MazeState>();
+    }
+    beam[0].push(state);
+
+    // search for each beam
+    for (int cnt = 0; cnt < beam_number; cnt++)
+    {
+        // search for each depth
+        for (int t = 0; t < beam_depth; t++)
+        {
+            auto &now_beam = beam[t];
+            auto &next_beam = beam[t + 1];
+            // search width
+            for (int i = 0; i < beam_width; i++)
+            {
+                if (now_beam.empty())
+                {
+                    break;
+                }
+                auto now_state = now_beam.top();
+                if (now_state.is_done())
+                {
+                    break;
+                }
+                now_beam.pop();
+
+                // search next state
+                auto legal_actions = now_state.legal_actions();
+                for (const auto &action : legal_actions)
+                {
+                    MazeState next_state = now_state;
+                    next_state.advance(action);
+                    next_state.evaluate_score();
+                    if (t == 0)
+                    {
+                        next_state.first_action_ = action;
+                    }
+                    next_beam.push(next_state);
+                }
+            }
+        }
+    }
+    // 最後から辿っていって最初のNullでないactionを返す
+    for (int t = beam_depth; t >= 0; t--)
+    {
+        const auto &now_beam = beam[t];
+        if (!now_beam.empty())
+        {
+            return now_beam.top().first_action_;
+        }
+    }
+    return -1;
+};
---
+++ cpp/src/ch03/chokudai_search.h
@@ -0,0 +1,13 @@
+
+#ifndef SRC_CH03_CHOKUDAI_SEARCH_H_
+#define SRC_CH03_CHOKUDAI_SEARCH_H_
+
+#include "maze-state.h"
+
+int chokudai_search_action(
+    const MazeState &state,
+    const int beam_width,
+    const int beam_depth,
+    const int beam_number){};
+
+#endif

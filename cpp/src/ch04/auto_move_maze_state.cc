2d4934f339d0df5cd3b7983e3bb837d10d4fa5d5
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-10 14:57:56 +0900

    add sample code

--- cpp/src/ch04/auto_move_maze_state.cc
+++ cpp/src/ch04/auto_move_maze_state.cc
@@ -1,1 +1,60 @@
-#include "auto-move-maze-state.h"
+#include <sstream>
+#include <random>
+#include "auto_move_maze_state.h"
+
+AutoMoveMazeState::AutoMoveMazeState(const int seed) : turn_(0),
+                                                       game_score_(0),
+                                                       evaluated_score_(0)
+{
+    auto mt_for_construct = std::mt19937(seed);
+    for (int y = 0; y < H; y++)
+    {
+        for (int x = 0; x < W; x++)
+        {
+            points_[y][x] = mt_for_construct() % 9 + 1;
+        }
+    }
+}
+
+void AutoMoveMazeState::set_character(const int character_id,
+                                      const int y,
+                                      const int x)
+{
+    this->characters_[character_id].y_ = y;
+    this->characters_[character_id].x_ = x;
+}
+
+bool AutoMoveMazeState::is_done() const
+{
+    return this->turn_ == END_TURN;
+}
+
+std::string AutoMoveMazeState::to_string() const
+{
+    std::stringstream ss;
+    ss << "turn:\t" << this->turn_ << "\n";
+    ss << "score:\t" << this->game_score_ << "\n";
+    for (int h = 0; h < H; h++)
+    {
+        for (int w = 0; w < W; w++)
+        {
+            for (Coord character_ : characters_)
+            {
+                if (character_.y_ == h && character_.x_ == w)
+                {
+                    ss << '@';
+                }
+            }
+            if (this->points_[h][w] > 0)
+            {
+                ss << points_[h][w];
+            }
+            else
+            {
+                ss << '.';
+            }
+        }
+        ss << "\n";
+    }
+    return ss.str();
+}
--- cpp/src/ch04/auto_move_maze_state.h
+++ cpp/src/ch04/auto_move_maze_state.h
@@ -1,15 +1,37 @@
 #ifndef SRC_CH04_AUTO_MOVE_MAZE_STATE_H_
 #define SRC_CH04_AUTO_MOVE_MAZE_STATE_H_
 
+#include <iostream>
+
 constexpr const int H = 5;
 constexpr const int W = 5;
 constexpr int END_TURN = 5;
 constexpr int CHARACTER_N = 3;
 
+struct Coord
+{
+    /* data */
+    int y_;
+    int x_;
+    Coord(const int y = 0, const int x = 0) : y_(y), x_(x) {}
+};
+
+using ScoreType = int64_t;
 class AutoMoveMazeState
 {
 private:
-    int points
-}
+    int points_[H][W] = {};
+    int turn_;
+    Coord characters_[CHARACTER_N] = {};
+
+public:
+    int game_score_;
+    ScoreType evaluated_score_;
+    AutoMoveMazeState(const int seed);
+    bool is_done() const;
+    std::string to_string() const;
+    void set_character(const int character_id, const int y, const int x);
+    ScoreType get_score(bool is_print = false) const;
+};
 
 #endif

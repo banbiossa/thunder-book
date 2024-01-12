ba2448a5091af2600f09658241ec3a698fe6018b
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-12 18:15:42 +0900

    add average score

---
+++ python/src/thunder_book/ch04/average_score.py
@@ -0,0 +1,56 @@
+import random
+from typing import Callable
+
+from tqdm import tqdm
+import fire
+
+from thunder_book.ch04.auto_move_maze_state import MazeState as State
+from thunder_book.ch04.hill_climb import hill_climb
+from thunder_book.ch04.random_action import random_action
+from thunder_book.ch04.simulated_annealing import simulated_annealing
+
+
+def test_ai_score(
+    name: str,
+    action_func: Callable[[State], State],
+    game_number: int,
+):
+    score_mean = 0
+    for _ in tqdm(range(game_number)):
+        state = State(random.randint(0, 100000))
+        last_state = action_func(state)
+        score = last_state.get_score()
+        score_mean += score
+    score_mean /= game_number
+    print(f"score of {name}: {score_mean:.2f}")
+
+
+def run(simulate_number=10000, game_number=100):
+    GAMES_TYPE = list[tuple[str, Callable[[State], State]]]
+    games: GAMES_TYPE = [
+        ("random_action", random_action),
+        ("hill_climb", lambda state: hill_climb(state, simulate_number)),
+        (
+            "simulated_annealing",
+            lambda state: simulated_annealing(
+                state,
+                simulate_number,
+                start_temp=500,
+                end_temp=10,
+            ),
+        ),
+    ]
+
+    # print what to play
+    print(
+        f"average of {game_number} games for {[name for name, _ in games]} in {simulate_number} simulations"
+    )
+
+    # play
+    for name, action_func in games:
+        print(f"play {name}")
+        test_ai_score(name, action_func, game_number)
+
+
+if __name__ == "__main__":
+    fire.Fire(run)

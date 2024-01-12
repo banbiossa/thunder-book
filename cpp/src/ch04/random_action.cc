aee0bd936d480642c62e8b6c3ec281a6be2baf4c
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-12 16:10:18 +0900

    remove

--- cpp/src/ch04/random_action.cc
+++ cpp/src/ch04/random_action.cc
@@ -15,12 +15,3 @@ State random_action(const State &state)
     }
     return now_state;
 }
-
-// int main()
-// {
-//     const auto &ai = StringAIPair(
-//         "random_action", [&](const State &state)
-//         { return random_action(state); });
-//     play_game(ai, 0);
-//     return 0;
-// }

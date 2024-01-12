2e7e4efdabe7fc16528b40266b2374889f21dfa1
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-12 15:19:48 +0900

    hill climb

--- python/src/thunder_book/ch04/hill_climb.py
+++ python/src/thunder_book/ch04/hill_climb.py
@@ -1,2 +1,16 @@
-from thunder_book.ch04 import constants
 from thunder_book.ch04.auto_move_maze_state import MazeState as State
+
+
+def hill_climb(state: State, number: int) -> State:
+    now_state = state.copy()
+    now_state.init_characters()
+
+    best_score = now_state.get_score()
+    for _ in range(number):
+        next_state = now_state.copy()
+        next_state.transition()
+        next_score = next_state.get_score()
+        if next_score > best_score:
+            best_score = next_score
+            now_state = next_state
+    return now_state

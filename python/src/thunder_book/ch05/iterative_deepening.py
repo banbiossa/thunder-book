f0eae9d2120e6cd1596ca35ff0018e8b510eb922
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-17 23:08:19 +0900

    final version

--- python/src/thunder_book/ch05/iterative_deepening.py
+++ python/src/thunder_book/ch05/iterative_deepening.py
@@ -10,16 +10,16 @@ from thunder_book.ch05 import constants
 def iterative_deepening_action(state: State, time_threshold: int):
     time_keeper = TimeKeeper(time_threshold)
     best_action = 0
-    for depth in range(1, 1_000):
+    # depth を大きくすると (e.g. 1000) なぜかバグる, 本来 END_TURN までやれば良いよね？
+    for depth in range(1, constants.END_TURN * 2):
         best_action = alpha_beta_action(state, depth=depth, time_keeper=time_keeper)
-        if time_keeper.is_time_over() or depth > constants.END_TURN * 2:
-            # depth を放置しているとなぜかバグる
+        if time_keeper.is_time_over():
             # print(f"depth: {depth} with time {time_keeper.time_threshold}")
             break
     return best_action
 
 
-def compare_iterative_deepening(a: int = 30, b: int = 1):
+def compare_iterative_deepening(a: int = 10, b: int = 1):
     print(f"compare iterative deepening {a}ms vs. {b}ms")
     iterative_deepening_a = lambda state: iterative_deepening_action(state, a)
     iterative_deepening_b = lambda state: iterative_deepening_action(state, b)

d2b91fceddecd28455f417b725d791f0c1ead043
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-21 18:55:03 +0900

    fmt

--- cpp/src/ch05/time_keeper.h
+++ cpp/src/ch05/time_keeper.h
@@ -14,9 +14,7 @@ private:
 public:
     TimeKeeper(const int64_t &time_threshold)
         : start_time_(std::chrono::high_resolution_clock::now()),
-          time_threshold_(time_threshold)
-    {
-    }
+          time_threshold_(time_threshold) {}
     bool is_time_over() const;
     float get_elapsed_time();
 };

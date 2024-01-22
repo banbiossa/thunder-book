c334b232174f5f2ad0f47ffb657a51e10836e7bf
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-22 11:25:43 +0900

    add compare mcts

---
+++ cpp/src/ch05/play_compare_mcts.cc
@@ -0,0 +1,25 @@
+#include "monte_carlo_tree_search.h"
+#include "win_rate.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    int a = 100;
+    int b = 10;
+    AIFunction mcts_action_a = [&](const State &state)
+    {
+        return mcts_action(state, a);
+    };
+    AIFunction mcts_action_b = [&](const State &state)
+    {
+        return mcts_action(state, b);
+    };
+
+    int num_games = 1000;
+    AIFunction actions_wb[2] = {mcts_action_a, mcts_action_b};
+    double win_rate = games_black_and_white(num_games, actions_wb);
+    cout << "win rate of mcts " << a << " over " << b
+         << " in " << num_games << " games is " << win_rate << endl;
+    return 0;
+}

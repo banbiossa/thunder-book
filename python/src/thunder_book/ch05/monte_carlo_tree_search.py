4f2217868724e26799189e6bbe277611196a3c62
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-22 18:58:54 +0900

    add play compare

--- python/src/thunder_book/ch05/monte_carlo_tree_search.py
+++ python/src/thunder_book/ch05/monte_carlo_tree_search.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 import numpy as np
+import fire
 
 from thunder_book.ch05.maze_state import AlternateMazeState as State
-from thunder_book.ch05.monte_carlo import playout
+from thunder_book.ch05.monte_carlo import playout, primitive_monte_carlo_action
 from thunder_book.ch05 import constants
+from thunder_book.ch05.average_score import average_score
 
 
 def mcts_action(state: State, playout_number: int):
@@ -68,3 +70,21 @@ class Node:
         t = sum(c.n for c in self.child_nodes)
         ucb1 = [c.ucb1(t) for c in self.child_nodes]
         return self.child_nodes[np.argmax(ucb1)]
+
+
+def mcts_vs_monte_carlo(num_playout: int = 30):
+    mcts_action_f = lambda state: mcts_action(state, num_playout)
+    monte_carlo_action_f = lambda state: primitive_monte_carlo_action(
+        state, num_playout
+    )
+    num_games = 100
+    win_rate = average_score(
+        num_games=num_games, actions_wb=(mcts_action_f, monte_carlo_action_f)
+    )
+    print(
+        f"win rate of MCTS vs Monte Carlo in {num_games=}, {num_playout=}: {win_rate}"
+    )
+
+
+if __name__ == "__main__":
+    fire.Fire(mcts_vs_monte_carlo)

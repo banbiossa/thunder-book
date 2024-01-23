2fb4a77dfe581de9e1c234a688cff30c720266b6
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-23 21:11:31 +0900

    change name

---
+++ cpp/src/ch05/play_time_thunder_vs_iterative_deepening.cc
@@ -0,0 +1,31 @@
+#include "win_rate.h"
+#include "thunder_search.h"
+#include "iterative_deepening.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    int num_games = 100;
+    int64_t time_threshold = 1;
+
+    AIFunction thunder_search_timebound = [&](const State &state)
+    {
+        return thunder::thunder_search_action_with_timekeeper(
+            state, time_threshold);
+    };
+    AIFunction iterative_deepening_timebound = [&](const State &state)
+    { return iterative_deepening_action(state, time_threshold); };
+
+    AIFunction actions_wb[2] = {thunder_search_timebound,
+                                iterative_deepening_timebound};
+
+    float win_rate = games_black_and_white(num_games, actions_wb);
+
+    cout << "win rate of thunder vs iterative deepening under "
+         << time_threshold << "ms is " << win_rate
+         << " over " << num_games << " games." << endl;
+
+    return 0;
+}

e995ec8b7627b0a63dce92ef8614ebefc4c07c0b
shota shimizu <shota.shimizu@legalontech.jp>
2024-01-23 11:00:35 +0900

    add final

--- python/src/thunder_book/ch05/constants.py
+++ python/src/thunder_book/ch05/constants.py
@@ -1,6 +1,8 @@
-H = 5
-W = 5
-END_TURN = 10
+from typing import Final
 
-C = 1.0
-EXPAND_THRESHOLD = 10
+H: Final = 5
+W: Final = 5
+END_TURN: Final = 10
+
+C: Final = 1.0
+EXPAND_THRESHOLD: Final = 10

f3adec036ffeffb9ecd836160e42184377bf8f59
shota shimizu <shota.shimizu@legalontech.jp>
2024-02-01 22:20:37 +0900

    move implementation

--- cpp/src/ch06/random_action.cc
+++ cpp/src/ch06/random_action.cc
@@ -9,3 +9,13 @@ int random_action(const State &state, const int player_id)
     auto legal_actions = state.legal_actions(player_id);
     return legal_actions[mt_for_action() % (legal_actions.size())];
 }
+
+namespace alternate
+{
+
+    int random_action(const AlternateMazeState &state)
+    {
+        auto legal_actions = state.legal_actions();
+        return legal_actions[mt_for_action() % legal_actions.size()];
+    }
+}

21ca76b0dd0b5a39a65031ac36e059e5b505a750
shota shimizu <shota.shimizu@legalontech.jp>
2024-02-05 23:13:01 +0900

    add mcts skelton for cc

--- cpp/src/ch06/mcts.h
+++ cpp/src/ch06/mcts.h
@@ -1,40 +1,18 @@
 #ifndef SRC_CH06_MCTS_H_
 #define SRC_CH06_MCTS_H_
 
-#include "alternate_maze_state.h"
+#include "maze_state.h"
 
-namespace alternate
+template <typename T>
+class BaseNode
 {
-    using State = AlternateMazeState;
+public:
+    State state;
+    virtual ~BaseNode() = default;
 
-    double playout(State *state);
-
-    constexpr const double C = 1.;
-    constexpr const int EXPAND_THRESHOLD = 10;
-
-    class Node
-    {
-    private:
-        State state_;
-        double w_;
-
-        // util
-        double ucb1(double t) const;
-
-    public:
-        Node(State &state);
-        std::vector<Node> child_nodes_;
-        double n_;
-
-        double explore();
-        void expand();
-        Node &next_child_node();
-    };
-
-    int mcts_action(const SimultaneousMazeState &base_state,
-                    const int player_id,
-                    const int playout_number);
-
-}
+    // override
+    virtual void expand();
+    virtual double explore();
+};
 
 #endif
---
+++ cpp/src/ch06/mcts_alternate.h
@@ -0,0 +1,40 @@
+#ifndef SRC_CH06_MCTS_ALTERNATE_H_
+#define SRC_CH06_MCTS_ALTERNATE_H_
+
+#include "alternate_maze_state.h"
+
+namespace alternate
+{
+    using State = AlternateMazeState;
+
+    double playout(State *state);
+
+    constexpr const double C = 1.;
+    constexpr const int EXPAND_THRESHOLD = 10;
+
+    class Node
+    {
+    private:
+        State state_;
+        double w_;
+
+        // util
+        double ucb1(double t) const;
+
+    public:
+        Node(State &state);
+        std::vector<Node> child_nodes_;
+        double n_;
+
+        double explore();
+        void expand();
+        Node &next_child_node();
+    };
+
+    int mcts_action(const SimultaneousMazeState &base_state,
+                    const int player_id,
+                    const int playout_number);
+
+}
+
+#endif

247d702aed4bad6d6a74703e8c4925ca8ce0636b
shota shimizu <shota.shimizu@legalontech.jp>
2024-02-05 23:16:36 +0900

    add virtual functions

--- cpp/src/ch06/mcts.h
+++ cpp/src/ch06/mcts.h
@@ -10,9 +10,25 @@ public:
     State state;
     virtual ~BaseNode() = default;
 
+    double ucb1(double t) const;
+
     // override
     virtual void expand();
     virtual double explore();
+};
+
+class EvenNode : public BaseNode<OddNode>
+{
+public:
+    void expand() override;
+    double explore() override;
+};
+
+class OddNode : public BaseNode<EvenNode>
+{
+public:
+    void expand() override;
+    double explore() override;
 };
 
 #endif

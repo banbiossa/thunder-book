c29f8860c5c9afff8f629778a10ed133c13b2b94
shota shimizu <shota.shimizu@legalontech.jp>
2024-02-06 10:49:49 +0900

    add playout as class

--- cpp/src/ch06/mcts.h
+++ cpp/src/ch06/mcts.h
@@ -3,23 +3,49 @@
 
 #include "maze_state.h"
 
+// make playout a class to avoid
+// having to remember to copy
+// double playout(State *state);
+
+class Playout
+{
+private:
+    State state_;
+
+public:
+    Playout(const State &state) : state_(state){};
+    double playout();
+};
+
 template <typename T>
 class BaseNode
 {
-public:
-    State state;
-    virtual ~BaseNode() = default;
+private:
+    State state_;
+    double w_;
 
+    // util
     double ucb1(double t) const;
+    void _increment(double value);
+
+public:
+    std::vector<T> child_nodes_;
+    int n_;
+
+    BaseNode(const State &state) : state_(state), w_(0), n_(0);
+    T &next_child_node();
 
     // override
     virtual void expand();
     virtual double explore();
+
+    // virtual ~BaseNode() = default;
 };
 
 class EvenNode : public BaseNode<OddNode>
 {
 public:
+    EvenNode(const State &state) : BaseNode(state){};
     void expand() override;
     double explore() override;
 };
@@ -27,6 +53,7 @@ public:
 class OddNode : public BaseNode<EvenNode>
 {
 public:
+    OddNode(const State &state) : BaseNode(state){};
     void expand() override;
     double explore() override;
 };

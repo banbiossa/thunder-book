f3ca6c8cb42d5ac5499c21e27e443934ba5d7670
shota shimizu <shota.shimizu@legalontech.jp>
2024-02-12 11:50:15 +0900

    use shared_ptr

--- cpp/src/ch07/beam_search.cc
+++ cpp/src/ch07/beam_search.cc
@@ -1,47 +1,63 @@
+#include <memory>
 #include <unordered_set>
 #include <queue>
+#include <functional>
 #include "beam_search.h"
+
+struct StateComparator
+{
+    bool operator()(const std::shared_ptr<State> &lhs,
+                    const std::shared_ptr<State> &rhs) const
+    {
+        return *lhs < *rhs;
+    }
+};
+
+using Beam = std::priority_queue<std::shared_ptr<State>,
+                                 std::vector<std::shared_ptr<State>>,
+                                 StateComparator>;
 
 int beam_search_action(const State &initial_state,
                        const int beam_width,
                        const int beam_depth,
                        bool use_zobrist_hash)
 {
-    State best_state = initial_state;
+    auto best_state = std::make_shared<State>(initial_state);
 
-    std::priority_queue<State> beam;
-    beam.push(initial_state);
+    Beam beam;
+    beam.push(std::make_shared<State>(initial_state));
     auto hash_check = std::unordered_set<uint64_t>();
 
     for (int d = 0; d < beam_depth; d++)
     {
-        std::priority_queue<State> next_beam;
+        Beam next_beam;
         for (int w = 0; w < beam_width; w++)
         {
             if (beam.empty())
                 break;
-            State state = beam.top();
+            auto state = beam.top();
             beam.pop();
-            auto legal_actions = state.legal_actions();
+            auto legal_actions = state->legal_actions();
             for (const auto &action : legal_actions)
             {
-                State next_state = state;
-                next_state.advance(action);
+                auto next_state = std::make_shared<State>(*state);
+                // auto next_state =
+                next_state->advance(action);
                 // conditional use of hash, skip if hash hit
-                if (use_zobrist_hash && d >= 1 && hash_check.count(next_state.hash_) > 0)
+                if (use_zobrist_hash && d >= 1 && hash_check.count(next_state->hash_) > 0)
                     continue;
-                hash_check.emplace(next_state.hash_);
+                hash_check.emplace(next_state->hash_);
 
-                next_state.evaluate_score();
+                next_state->evaluate_score();
                 if (d == 0)
-                    next_state.first_action_ = action;
+                    next_state->first_action_ = action;
                 next_beam.push(next_state);
             }
         }
-        beam = next_beam;
+        beam = std::move(next_beam);
         best_state = beam.top();
-        if (best_state.is_done())
+        if (best_state->is_done())
             break;
     }
-    return best_state.first_action_;
+    return best_state->first_action_;
 }

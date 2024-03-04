381763f56a8ab9515ec0511ffdd5c28aceecc9a2
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-04 18:59:37 +0900

    run times

--- rust/src/bin/ch05_time.rs
+++ rust/src/bin/ch05_time.rs
@@ -1,1 +1,42 @@
-fn main() {}
+use std::sync::Arc;
+
+use search::ch05::alpha_beta;
+use search::ch05::game;
+use search::ch05::maze_state;
+use search::ch05::mini_max;
+
+fn main() {
+    pub const PARAMS: maze_state::MazeParams = maze_state::MazeParams {
+        height: 3,
+        width: 3,
+        end_turn: 10,
+    };
+
+    let num_states = 100;
+    let states = game::sample_states(num_states, 0, PARAMS);
+
+    struct ActionName {
+        action_func: Arc<maze_state::ActionFunc>,
+        name: String,
+    }
+    let depth = PARAMS.end_turn;
+
+    let action_names = vec![
+        ActionName {
+            action_func: mini_max::mini_max_arc(depth),
+            name: format!("mini max depth {depth}"),
+        },
+        ActionName {
+            action_func: alpha_beta::alpha_beta_arc(depth),
+            name: format!("alpa beta depth {depth}"),
+        },
+    ];
+
+    for action_name in action_names {
+        let run_time = game::how_fast(action_name.action_func, &states);
+        println!(
+            "{} took {run_time} ms for {num_states} states",
+            action_name.name
+        );
+    }
+}

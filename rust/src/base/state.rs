833167381fe6136ccee071ca84b4c97ce7ee1371
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-11 11:01:24 +0900

    start moving traits to base

--- rust/src/base/state.rs
+++ rust/src/base/state.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 pub trait State: Clone {
     type Action;
     fn is_done(&self) -> bool;
@@ -12,16 +14,52 @@ pub struct MazeParams {
     pub end_turn: usize,
 }
 
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Character {
+    y: usize, // y coming first is important
+    x: usize,
+    mark: String,
+}
+
+/// base struct holds state of game
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct StateData {
+    character: Character,
+    game_score: usize,
+    // dims points[H][W]
+    points: Vec<Vec<usize>>,
+    turn: usize,
+    evaluated_score: usize,
+    first_action: Option<usize>, // will be set during explore
+    params: MazeParams,
+}
+
+impl PartialOrd for StateData {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for StateData {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.evaluated_score.cmp(&other.evaluated_score)
+    }
+}
+
 pub trait SinglePlayerState: Clone + Ord {
     fn new(seed: u64, params: MazeParams) -> Self;
     fn legal_actions(&self) -> Vec<usize>;
     fn advance(&mut self, action: usize);
-    fn set_first_action(&mut self, action: usize);
-    fn get_first_action(&self) -> usize;
     fn evaluate_score(&mut self);
     fn is_done(&self) -> bool;
     fn to_string(&self) -> String;
+    fn set_first_action(&mut self, action: usize);
+    fn get_first_action(&self) -> usize;
     fn get_game_score(&self) -> usize;
+    fn get_evaluated_score(&self) -> usize;
+    fn get_character(&self) -> &Character;
+    fn get_points(&self) -> &Vec<Vec<usize>>;
+    fn get_params(&self) -> &MazeParams;
 }
 
 pub type ActionFunc<T> = Box<dyn Fn(&T) -> usize>;

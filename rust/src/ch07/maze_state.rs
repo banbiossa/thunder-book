97cab1bd1e25203edc1a7a876f30fcff373b40c3
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-16 15:39:55 +0900

    update test

--- rust/src/ch07/maze_state.rs
+++ rust/src/ch07/maze_state.rs
@@ -1,17 +1,16 @@
-use crate::base::state::{Character, MazeParams, SinglePlayerState};
+use crate::{
+    base::state::{
+        single_player_state_portrait, Character, MazeParams, SinglePlayerState,
+    },
+    ch03::maze_state::NumberCollectingGame,
+};
 
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct WallMazeState {
+    state: NumberCollectingGame,
     walls: Vec<Vec<usize>>,
-    points: Vec<Vec<usize>>,
-    first_action: Option<usize>,
-    character: Character,
-    params: MazeParams,
-    turn: usize,
-    evaluated_score: isize,
-    game_score: usize,
 }
 
 impl PartialOrd for WallMazeState {
@@ -22,88 +21,55 @@ impl PartialOrd for WallMazeState {
 
 impl Ord for WallMazeState {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.evaluated_score.cmp(&other.evaluated_score)
+        self.get_evaluated_score().cmp(&other.get_evaluated_score())
     }
 }
 
+#[portrait::fill(portrait::delegate(NumberCollectingGame; self.state))]
 impl SinglePlayerState for WallMazeState {
     fn new(seed: u64, params: MazeParams) -> Self {
-        let character = Character::new(&params, seed);
-        let walls = Self::init_wall(&params, &character, seed);
-        let points = Self::init_points(&params, &character, seed);
+        let state = NumberCollectingGame::new(seed, params.clone());
+        let walls = Self::init_wall(&params, state.get_character(), seed);
 
-        WallMazeState {
-            walls,
-            evaluated_score: 0,
-            first_action: None,
-            character,
-            points,
-            params,
-            turn: 0,
-            game_score: 0,
-        }
+        WallMazeState { state, walls }
     }
 
     fn legal_actions(&self) -> Vec<usize> {
         (0..4)
             .filter(|&action| {
-                let ty = self.character.y as isize + Self::DY[action];
-                let tx = self.character.x as isize + Self::DX[action];
+                let ty = self.get_character().y as isize + Self::DY[action];
+                let tx = self.get_character().x as isize + Self::DX[action];
                 ty >= 0
-                    && (ty as usize) < self.params.height
+                    && (ty as usize) < self.get_params().height
                     && tx >= 0
-                    && (tx as usize) < self.params.width
+                    && (tx as usize) < self.get_params().width
                     && self.walls[ty as usize][tx as usize] == 0
             })
             .collect()
     }
 
-    fn is_done(&self) -> bool {
-        self.turn >= self.params.end_turn
-    }
-
-    fn evaluate_score(&mut self) {
-        self.evaluated_score = self.game_score as isize;
-    }
-
-    /// moves game one action forward
-    fn advance(&mut self, action: usize) -> usize {
-        let character = &mut self.character;
-        character.y =
-            (character.y as isize + Self::DY[action] as isize) as usize;
-        character.x =
-            (character.x as isize + Self::DX[action] as isize) as usize;
-        let point = self.points[character.y][character.x];
-        self.game_score += point;
-        self.points[character.y][character.x] = 0;
-        self.turn += 1;
-        point
-    }
-
-    fn set_first_action(&mut self, action: usize) {
-        if self.first_action.is_none() {
-            self.first_action = Some(action);
-        }
-    }
-
-    fn get_first_action(&self) -> usize {
-        self.first_action.unwrap()
-    }
     /// utility to show state of game
     fn to_string(&self) -> String {
+        let turn = self.get_turn();
+        let game_score = self.get_game_score();
+        let params = self.get_params();
+        let character = self.get_character();
+        let points = self.get_points();
+        let walls = self.get_walls();
+
         let mut ss = String::from("");
 
-        ss += &format!("turn:\t{}\n", self.turn);
-        ss += &format!("score:\t{}\n", self.game_score);
-        for y in 0..self.params.height {
+        ss += &format!("turn:\t{}\n", turn);
+        ss += &format!("score:\t{}\n", game_score);
+        for y in 0..params.height {
             ss += "\n";
-            for x in 0..self.params.width {
-                if self.character.y == y && self.character.x == x {
+            for x in 0..params.width {
+                if character.y == y && character.x == x {
                     ss += "@";
-                } else if self.walls[y][x] == 1 {
+                } else if walls[y][x] == 1 {
                     ss += "#";
-                } else if self.points[y][x] > 0 {
-                    ss += &format!("{}", self.points[y][x]);
+                } else if points[y][x] > 0 {
+                    ss += &format!("{}", points[y][x]);
                 } else {
                     ss += ".";
                 }
@@ -113,70 +79,11 @@ impl SinglePlayerState for WallMazeState {
 
         ss
     }
-
-    fn get_game_score(&self) -> usize {
-        self.game_score
-    }
-
-    fn set_game_score(&mut self, score: usize) {
-        self.game_score = score;
-    }
-
-    fn get_character(&self) -> &Character {
-        &self.character
-    }
-
-    fn get_evaluated_score(&self) -> isize {
-        self.evaluated_score
-    }
-
-    fn set_evaluated_score(&mut self, score: isize) {
-        self.evaluated_score = score;
-    }
-
-    fn get_params(&self) -> &MazeParams {
-        &self.params
-    }
-
-    fn get_points(&self) -> &Vec<Vec<usize>> {
-        &self.points
-    }
-
-    fn remove_points(&mut self, y: usize, x: usize) {
-        self.points[y][x] = 0
-    }
-
-    fn get_turn(&self) -> usize {
-        self.turn
-    }
-
-    fn set_turn(&mut self, turn: usize) {
-        self.turn = turn;
-    }
 }
 
 impl WallMazeState {
     pub const DX: [isize; 4] = [1, -1, 0, 0];
     pub const DY: [isize; 4] = [0, 0, 1, -1];
-
-    fn init_points(
-        params: &MazeParams,
-        character: &Character,
-        seed: u64,
-    ) -> Vec<Vec<usize>> {
-        // make points
-        let mut rng = StdRng::seed_from_u64(seed);
-        let mut points = vec![vec![0; params.width]; params.height];
-        for y in 0..params.height {
-            for x in 0..params.width {
-                if y == character.y && x == character.x {
-                    continue;
-                }
-                points[y][x] = rng.gen_range(0..=9);
-            }
-        }
-        points
-    }
 
     fn init_wall(
         params: &MazeParams,
@@ -249,15 +156,15 @@ mod tests {
         let mut state = setup();
         state.advance(1);
         assert_eq!(
-            state.character,
-            Character {
+            state.get_character(),
+            &Character {
                 x: 0,
                 y: 0,
                 mark: "A".to_string()
             }
         );
-        assert_eq!(state.turn, 1);
-        assert_eq!(state.game_score, 7);
+        assert_eq!(state.get_turn(), 1);
+        assert_eq!(state.get_game_score(), 2);
     }
 
     #[test]
@@ -268,28 +175,13 @@ mod tests {
 turn:\t0
 score:\t0
 
-7@.22
-7#1##
-49251
-8#5##
-9#665
+2@711
+.#4##
+51825
+6#9##
+6#735
 ";
         assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn test_init_points() {
-        let params = setup_params();
-        let character = Character::new(&params, 0);
-        let points = WallMazeState::init_points(&params, &character, 0);
-        let expected = [
-            [7, 0, 0, 2, 2],
-            [7, 1, 1, 0, 4],
-            [4, 9, 2, 5, 1],
-            [8, 2, 5, 6, 2],
-            [9, 0, 6, 6, 5],
-        ];
-        assert_eq!(points, expected);
     }
 
     #[test]

285bf45b6760bd32efaf6a4f7bccd7bd49ca17a6
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-17 13:32:19 +0900

    add all funcs by myself

--- rust/src/ch07/bitstate.rs
+++ rust/src/ch07/bitstate.rs
@@ -1,6 +1,9 @@
-use crate::base::state::{MazeParams, SinglePlayerState};
+use crate::base::state::{
+    single_player_state_portrait, MazeParams, SinglePlayerState, Wall,
+};
 
 use super::zobrist_hash::ZobristState;
+use std::cmp::Ordering;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Mat {
@@ -98,8 +101,106 @@ impl Mat {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MultiBitsetState {
     state: ZobristState,
-    points: Mat,
-    walls: Mat,
+    points_mat: Mat,
+    walls_mat: Mat,
+}
+
+impl PartialOrd for MultiBitsetState {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for MultiBitsetState {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.get_evaluated_score().cmp(&other.get_evaluated_score())
+    }
+}
+
+impl Wall for MultiBitsetState {
+    fn get_walls(&self) -> &Vec<Vec<usize>> {
+        self.state.get_walls()
+    }
+}
+
+// #[portrait::fill(portrait::delegate(NearPointState; self.state.state))]
+impl SinglePlayerState for MultiBitsetState {
+    fn get_evaluated_score(&self) -> isize {
+        self.state.get_evaluated_score()
+    }
+    fn new(seed: u64, params: MazeParams) -> Self {
+        let state = ZobristState::new(seed, params.clone());
+        let mut points_mat = Mat::new(&params);
+        let mut walls_mat = Mat::new(&params);
+
+        for y in 0..params.height {
+            for x in 0..params.width {
+                if state.get_walls()[y][x] != 0 {
+                    walls_mat.set(y, x);
+                }
+                if state.get_points()[y][x] != 0 {
+                    points_mat.set(y, x);
+                }
+            }
+        }
+        MultiBitsetState {
+            state,
+            points_mat,
+            walls_mat,
+        }
+    }
+    // self.point_mat の更新必要じゃない？
+    fn remove_points(&mut self, y: usize, x: usize) {
+        self.state.remove_points(y, x);
+        self.points_mat.del(y, x);
+    }
+
+    // copy from state
+    fn advance(&mut self, action: usize) -> usize {
+        self.state.advance(action)
+    }
+    fn legal_actions(&self) -> Vec<usize> {
+        self.state.legal_actions()
+    }
+    fn evaluate_score(&mut self) {
+        self.state.evaluate_score()
+    }
+    fn is_done(&self) -> bool {
+        self.state.is_done()
+    }
+    fn to_string(&self) -> String {
+        self.state.to_string()
+    }
+    fn get_character(&self) -> &crate::base::state::Character {
+        self.state.get_character()
+    }
+    fn get_first_action(&self) -> usize {
+        self.state.get_first_action()
+    }
+    fn get_game_score(&self) -> usize {
+        self.state.get_game_score()
+    }
+    fn get_params(&self) -> &MazeParams {
+        self.state.get_params()
+    }
+    fn get_points(&self) -> &Vec<Vec<usize>> {
+        self.state.get_points()
+    }
+    fn get_turn(&self) -> usize {
+        self.state.get_turn()
+    }
+    fn set_evaluated_score(&mut self, score: isize) {
+        self.state.set_evaluated_score(score)
+    }
+    fn set_first_action(&mut self, action: usize) {
+        self.state.set_first_action(action)
+    }
+    fn set_game_score(&mut self, score: usize) {
+        self.state.set_game_score(score)
+    }
+    fn set_turn(&mut self, turn: usize) {
+        self.state.set_turn(turn)
+    }
 }
 
 // impl SinglePlayerState for MultiBitsetState {
@@ -130,12 +231,16 @@ pub struct MultiBitsetState {
 mod tests {
     use super::*;
 
-    fn setup() -> Mat {
-        let params = MazeParams {
+    fn setup_params() -> MazeParams {
+        MazeParams {
             height: 3,
             width: 3,
             end_turn: 3,
-        };
+        }
+    }
+
+    fn setup() -> Mat {
+        let params = setup_params();
         let mat = Mat {
             bits: vec![
                 // [0, 1, 0],
@@ -148,6 +253,17 @@ mod tests {
             params,
         };
         mat
+    }
+
+    #[test]
+    fn make_state() {
+        let params = MazeParams {
+            height: 5,
+            width: 5,
+            end_turn: 3,
+        };
+        let state = MultiBitsetState::new(0, params);
+        assert_eq!(state.get_evaluated_score(), 0);
     }
 
     #[test]

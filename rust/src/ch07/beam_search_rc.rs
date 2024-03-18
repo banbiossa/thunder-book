97a00a5b5648954f2da22b464aea55d051af6573
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-18 17:55:53 +0900

    use rc refcell

--- rust/src/ch07/beam_search_rc.rs
+++ rust/src/ch07/beam_search_rc.rs
@@ -1,43 +1,45 @@
 // same as beam_search_hash but use std::rc::Rc
 use crate::base::state::{HashableState, SinglePlayerState};
 use crate::base::{is_done, state::ActionFunc};
+use std::cell::RefCell;
 use std::collections::{BinaryHeap, HashSet};
 use std::rc::Rc;
 
-pub fn beam_search_hash_box<T: SinglePlayerState + HashableState>(
+pub fn beam_search_rc_action<T: SinglePlayerState + HashableState>(
     beam_width: usize,
     beam_depth: usize,
 ) -> ActionFunc<T> {
     Box::new(move |state| -> usize {
-        beam_search_with_hash(
-            Rc::new(state.clone()),
+        beam_search_hash_rc(
+            Rc::new(RefCell::new(state.clone())),
             beam_width,
             is_done::depth_stopper(beam_depth),
         )
     })
 }
 
-pub fn beam_search_hash_timed_box<T: SinglePlayerState + HashableState>(
+pub fn beam_search_timed_rc_action<T: SinglePlayerState + HashableState>(
     beam_width: usize,
     time_threshold_ms: u64,
 ) -> ActionFunc<T> {
     Box::new(move |state| -> usize {
-        beam_search_with_hash(
-            Rc::new(state.clone()),
+        beam_search_hash_rc(
+            Rc::new(RefCell::new(state.clone())),
             beam_width,
             is_done::time_stopper(time_threshold_ms),
         )
     })
 }
 
-fn beam_search_with_hash<T: SinglePlayerState + HashableState>(
-    initial_state: Rc<T>,
+fn beam_search_hash_rc<T: SinglePlayerState + HashableState>(
+    initial_state: Rc<RefCell<T>>,
     beam_width: usize,
     mut stop_condition: is_done::Stopper,
 ) -> usize {
-    let mut best_state = initial_state.clone();
+    let mut best_state = Rc::clone(&initial_state);
     let mut beam = BinaryHeap::new();
-    beam.push(initial_state.clone());
+    beam.push(Rc::clone(&initial_state));
+
     let mut hash_check = HashSet::new();
 
     while !stop_condition() {
@@ -49,61 +51,70 @@ fn beam_search_with_hash<T: SinglePlayerState + HashableState>(
             }
 
             let top_state = beam.pop().unwrap();
-            let legal_actions = top_state.legal_actions();
+            let legal_actions = top_state.borrow().legal_actions();
 
             for action in legal_actions {
-                let mut state = top_state.clone();
+                let mut state = top_state.borrow().clone();
                 state.advance(action);
 
                 if hash_check.contains(&state.get_hash()) {
                     continue;
                 }
-                hash_check.insert(state.get_hash());
 
+                hash_check.insert(state.get_hash());
                 state.evaluate_score();
                 state.set_first_action(action);
-                next_beam.push(Rc::new(state));
+
+                next_beam.push(Rc::new(RefCell::new(state)));
             }
         }
 
         beam = next_beam;
-        best_state = beam.peek().cloned().unwrap_or(best_state);
 
-        if best_state.is_done() {
+        if let Some(top_state) = beam.peek() {
+            if top_state.borrow().get_evaluated_score()
+                > best_state.borrow().get_evaluated_score()
+            {
+                best_state = Rc::clone(top_state);
+            }
+        }
+
+        if best_state.borrow().is_done() {
             break;
         }
     }
 
-    best_state.get_first_action()
+    let x = best_state.borrow().get_first_action();
+    x
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{base::state::MazeParams, ch07::zobrist_hash::ZobristState};
-    use std::rc::Rc;
 
-    fn setup() -> Rc<ZobristState> {
+    fn setup() -> ZobristState {
         let params = MazeParams {
             height: 5,
             width: 5,
             end_turn: 3,
         };
         let state = ZobristState::new(0, params);
-        Rc::new(state)
+        state
+        // Rc::new(RefCell::new(state))
     }
 
     #[test]
     fn test_beam_search_timed() {
         let state = setup();
-        let actual = beam_search_hash_timed_box(3, 1)(&state);
+        let actual = beam_search_timed_rc_action(3, 1)(&state);
         assert_eq!(actual, 0);
     }
 
     #[test]
     fn test_beam_search_with_hash() {
         let state = setup();
-        let actual = beam_search_hash_box(3, 3)(&state);
+        let actual = beam_search_rc_action(3, 3)(&state);
         assert_eq!(actual, 0);
     }
 }

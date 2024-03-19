4441e89a97366527e8ede88badcd519a8f6ae396
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-19 20:55:57 +0900

    add test

--- python/tests/ch08/test_random_action.py
+++ python/tests/ch08/test_random_action.py
@@ -1,3 +1,4 @@
+from thunder_book.ch08.bitset import ConnectFourBitset
 from thunder_book.ch08.maze_state import ConnectFourState, MazeParams
 from thunder_book.ch08.random_action import random_action
 
@@ -5,5 +6,12 @@ from thunder_book.ch08.random_action import random_action
 def test_random_action():
     params = MazeParams(width=7, height=6)
     state = ConnectFourState(params)
+    action = random_action(state)
+    assert action in state.legal_actions()
+
+
+def test_random_action_bitstate():
+    params = MazeParams(width=7, height=6)
+    state = ConnectFourBitset(params)
     action = random_action(state)
     assert action in state.legal_actions()

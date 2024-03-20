08a38a6816dc756f01a0f605069af92e9d28dc6f
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-20 23:42:47 +0900

    add alternate trait

---
+++ rust/src/base/alternate.rs
@@ -0,0 +1,18 @@
+use crate::base::game_result;
+
+#[derive(Debug, Clone)]
+pub struct MazeParams {
+    pub height: usize,
+    pub width: usize,
+    pub end_turn: usize,
+}
+
+pub trait AlternateState: Clone {
+    fn new(seed: u64, params: MazeParams) -> Self;
+    fn is_done(&self) -> bool;
+    fn advance(&mut self, action: usize);
+    fn legal_actions(&self) -> Vec<usize>;
+    fn to_string(&self) -> String;
+    // need to change this to -> f32
+    fn white_score(&self) -> game_result::GameResult;
+}
--- rust/src/base/mod.rs
+++ rust/src/base/mod.rs
@@ -1,3 +1,4 @@
+pub mod alternate;
 pub mod game_result;
 pub mod is_done;
 pub mod playout;

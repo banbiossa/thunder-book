fda6d52c8fc3b2e5659301b45465f1856debc18e
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-22 17:26:23 +0900

    add init character to fixture

--- python/tests/ch04/test_auto_move_maze_state.py
+++ python/tests/ch04/test_auto_move_maze_state.py
@@ -6,7 +6,9 @@ from thunder_book.ch04.auto_move_maze_state import Coord, MazeParams, MazeState
 @pytest.fixture
 def state():
     params = MazeParams(width=5, height=5, end_turn=4, num_characters=3)
-    return MazeState(1, params)
+    t_state = MazeState(1, params)
+    t_state.init_characters()
+    return t_state
 
 
 def test_make_state(state):
@@ -16,7 +18,6 @@ def test_make_state(state):
 
 
 def test_str(state):
-    state.init_characters()
     actual = str(state)
     expected = """\
 turn: 0
@@ -32,7 +33,12 @@ score: 0
 
 
 def test_init_characters(state):
-    state.init_characters()
     assert state.chracters[0] == Coord(x=4, y=0)
     assert state.chracters[1] == Coord(x=3, y=0)
     assert state.chracters[2] == Coord(x=3, y=1)
+
+
+def test_set_character(state):
+    assert state.chracters[0] == Coord(x=4, y=0)
+    state.set_character(0, 1, 1)
+    assert state.chracters[0] == Coord(x=1, y=1)

dd75c4e3f9661f1b4994a857573369366f813703
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-22 20:20:26 +0900

    update

--- python/tests/ch04/test_average_score.py
+++ python/tests/ch04/test_average_score.py
@@ -1,7 +1,11 @@
-from thunder_book.ch04.average_score import run_many
+from thunder_book.ch04.average_score import run, run_many
 from thunder_book.ch04.hill_climb import hill_climb
 
 
 def test_run_many():
     actual = run_many(lambda state: hill_climb(state, 10), 10)
     assert actual > 0
+
+
+def test_run():
+    run(num_games=1, num_simulate=1)

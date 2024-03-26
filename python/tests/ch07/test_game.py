86a7e2a4dffefe505e118a9f582d37e23d666918
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-26 16:56:37 +0900

    update

--- python/tests/ch07/test_game.py
+++ python/tests/ch07/test_game.py
@@ -1,19 +1,9 @@
-import pytest
-
 from thunder_book.ch07.game import BeamType, get_state, play_game, white_games
 from thunder_book.ch07.maze_state import (
     MazeParams,
 )
 from thunder_book.ch07.numpy_state import NumpyState
 from thunder_book.ch07.random_action import random_action
-
-BeamType, play_game, get_state, white_games
-
-
-@pytest.fixture
-def state() -> NumpyState:
-    params = MazeParams(height=3, width=5, end_turn=4)
-    return NumpyState(0, params)
 
 
 def test_get_state():

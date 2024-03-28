bad6a3d7bf0b261d11f370059391c4d98b0c6599
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-28 12:34:11 +0900

    use raw state

--- cpp/src/ch03/average_score.cc
+++ cpp/src/ch03/average_score.cc
@@ -9,10 +9,10 @@ using std::cout;
 using std::endl;
 
 void test_ai_score(const int game_number,
-                   const std::function<int(const MazeState &)> &action_func)
+                   const std::function<int(const MazeState &)> &action_func,
+                   const MazeParams &params)
 {
 
-    const MazeParams params = MazeParams{30, 30, 100};
     auto time_keeper = TimeKeeper(1000);
     std::mt19937 mt_for_construct(0);
     double score_mean = 0;
@@ -35,20 +35,21 @@ void test_ai_score(const int game_number,
 
 int main()
 {
+    const MazeParams params = MazeParams{30, 30, 100};
     int num_games = 100;
     cout << "random" << endl;
-    test_ai_score(num_games, random_action);
+    test_ai_score(num_games, random_action, params);
 
     cout << "greedy" << endl;
-    test_ai_score(num_games, greedy_action);
+    test_ai_score(num_games, greedy_action, params);
 
     cout << "beam width 2" << endl;
     // define a particial to pass to test_ai_score
     auto partial_beam_search_action = [&](const MazeState &state)
     {
-        return beam_search_action(state, 2, END_TURN);
+        return beam_search_action(state, 2, params.end_turn_);
     };
-    test_ai_score(num_games, partial_beam_search_action);
+    test_ai_score(num_games, partial_beam_search_action, params);
 
     cout << "beam width 5 with timekeeper 1ms" << endl;
     auto partial_beam_w_time_keeper = [&](const MazeState &state)
@@ -56,7 +57,7 @@ int main()
         return beam_search_action_with_time_threshold(
             state, /* beam_width */ 5, /* time threshold */ 1);
     };
-    test_ai_score(num_games, partial_beam_w_time_keeper);
+    test_ai_score(num_games, partial_beam_w_time_keeper, params);
 
     cout << "beam width 5 with timekeeper 10ms" << endl;
     auto partial_beam_w_time_keeper_10 = [&](const MazeState &state)
@@ -64,23 +65,23 @@ int main()
         return beam_search_action_with_time_threshold(
             state, /* beam_width */ 5, /* time threshold */ 10);
     };
-    test_ai_score(num_games, partial_beam_w_time_keeper_10);
+    test_ai_score(num_games, partial_beam_w_time_keeper_10, params);
 
     cout << "chokudai search with timekeeper 1ms" << endl;
     auto particial_chokudai_search_with_time_threshold_1 = [&](const MazeState &state)
     {
         return chokudai_search_action_with_time_threshold(
-            state, /*beam width */ 1, /* beam_depth */ END_TURN, /* time */ 1);
+            state, /*beam width */ 1, /* beam_depth */ params.end_turn_, /* time */ 1);
     };
-    test_ai_score(num_games, particial_chokudai_search_with_time_threshold_1);
+    test_ai_score(num_games, particial_chokudai_search_with_time_threshold_1, params);
 
     cout << "chokudai search with timekeeper 10ms" << endl;
     auto particial_chokudai_search_with_time_threshold_10 = [&](const MazeState &state)
     {
         return chokudai_search_action_with_time_threshold(
-            state, /*beam width */ 1, /* beam_depth */ END_TURN, /* time */ 10);
+            state, /*beam width */ 1, /* beam_depth */ params.end_turn_, /* time */ 10);
     };
-    test_ai_score(num_games, particial_chokudai_search_with_time_threshold_10);
+    test_ai_score(num_games, particial_chokudai_search_with_time_threshold_10, params);
 
     return 0;
 }

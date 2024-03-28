e8f2d0a78375c65f980a47a6d846b3e754048b2a
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-28 18:25:31 +0900

    clean

--- cpp/src/ch04/auto_move_maze_state.cc
+++ cpp/src/ch04/auto_move_maze_state.cc
@@ -37,13 +37,11 @@ std::string AutoMoveMazeState::to_string() const
     ss << "\n";
     ss << "turn: " << this->turn_ << "\n";
     ss << "score: " << this->game_score_ << "\n";
-    auto board_chars = std::vector<std::vector<char>>(H, std::vector<char>(W, '.'));
     for (int h = 0; h < H; h++)
     {
         for (int w = 0; w < W; w++)
         {
             bool is_written = false; // この座標に書く文字が決定したか
-
             for (const auto &character : this->characters_)
             {
                 if (character.y_ == h && character.x_ == w)
@@ -52,23 +50,17 @@ std::string AutoMoveMazeState::to_string() const
                     is_written = true;
                     break;
                 }
-                board_chars[character.y_][character.x_] = '@';
             }
             if (!is_written)
             {
                 if (this->points_[h][w] > 0)
-                {
                     ss << points_[h][w];
-                }
                 else
-                {
                     ss << '.';
-                }
             }
         }
         ss << '\n';
     }
-
     return ss.str();
 }
 

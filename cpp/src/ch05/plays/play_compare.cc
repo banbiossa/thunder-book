c6117029e898b6fcf486a82798d92e27bb88bcce
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-29 15:12:17 +0900

    add play compare

--- cpp/src/ch05/plays/play_compare.cc
+++ cpp/src/ch05/plays/play_compare.cc
@@ -4,6 +4,10 @@
 #include "src/ch05/iterative_deepening.h"
 #include "src/ch05/mini_max.h"
 #include "src/ch05/alpha_beta.h"
+#include "src/ch05/random_action.h"
+#include "src/ch05/monte_carlo.h"
+#include "src/ch05/monte_carlo_tree_search.h"
+#include "src/ch05/thunder_search.h"
 #include "src/util.h"
 
 using namespace std;
@@ -19,15 +23,23 @@ int main()
     // make the above into a struct or tuple to allow calls in a loop
     std::vector<ActionName> action_names = {
         ActionName{
+            .name = "random vs random",
             .action_funcs = {
                 [](const State &state)
-                { return iterative_deepening_action(state, 10); },
+                { return random_action(state); },
                 [](const State &state)
-                {
-                    return iterative_deepening_action(state, 1);
-                },
+                { return random_action(state); },
             },
-            .name = "iterative deepening 10ms vs. 1ms",
+        },
+        ActionName{
+            .name = "mini max vs random",
+            .action_funcs = {
+                [](const State &state)
+                { return mini_max_action(state, END_TURN); },
+                [](const State &state)
+                { return random_action(state); },
+            },
+
         },
         ActionName{
             .action_funcs = {
@@ -38,16 +50,111 @@ int main()
             },
             .name = "mini max vs. alpha-beta",
         },
+        ActionName{
+            .action_funcs = {
+                [](const State &state)
+                { return iterative_deepening_action(state, 2); },
+                [](const State &state)
+                {
+                    return iterative_deepening_action(state, 1);
+                },
+            },
+            .name = "iterative deepening 2ms vs. 1ms",
+        },
+        ActionName{
+            .name = "monte carlo 3000 vs. random",
+            .action_funcs = {
+                [](const State &state)
+                { return primitive_monte_carlo_action(state, 3000); },
+                random_action,
+            },
+        },
+        ActionName{
+            .name = "monte carlo 3000 vs. monte carlo 30",
+            .action_funcs = {
+                [](const State &state)
+                { return primitive_monte_carlo_action(state, 3000); },
+                [](const State &state)
+                { return primitive_monte_carlo_action(state, 30); },
+            },
+        },
+        ActionName{
+            .name = "mcts 3000 vs monte carlo 3000",
+            .action_funcs = {
+                [](const State &state)
+                {
+                    return mcts_action(state, 3000);
+                },
+                [](const State &state)
+                {
+                    return primitive_monte_carlo_action(state, 3000);
+                },
+            },
+        },
+        ActionName{
+            .name = "mcts 3000 vs mcts 30",
+            .action_funcs = {
+                [](const State &state)
+                {
+                    return mcts_action(state, 3000);
+                },
+                [](const State &state)
+                {
+                    return mcts_action(state, 30);
+                },
+            },
+        },
+        ActionName{
+            .name = "thunder vs mcts 3000",
+            .action_funcs = {
+                [](const State &state)
+                {
+                    return thunder::thunder_search_action(state, 3000);
+                },
+                [](const State &state)
+                {
+                    return mcts_action(state, 3000);
+                },
+
+            },
+        },
+        ActionName{
+            .name = "thunder vs mcts 1ms",
+            .action_funcs = {
+                [](const State &state)
+                {
+                    return thunder::thunder_search_action_with_timekeeper(state, 1);
+                },
+                [](const State &state)
+                {
+                    return mcts_action_with_time_threshold(state, 1);
+                },
+            },
+        },
+        ActionName{
+            .name = "thunder vs iterative deepening 1ms",
+            .action_funcs = {
+                [](const State &state)
+                {
+                    return thunder::thunder_search_action_with_timekeeper(state, 1);
+                },
+                [](const State &state)
+                {
+                    return iterative_deepening_action(state, 1);
+                },
+            },
+        },
         //
     };
 
     log_to_file("| action | win_rate% | time |");
     log_to_file("| ------ | --------- | ---- |");
+    int num_games = 100;
     for (auto action_name : action_names)
     {
         cout << action_name.name << endl;
         auto start = std::chrono::system_clock::now();
-        float win_rate = games_black_and_white(100, action_name.action_funcs.data(), 10);
+        float win_rate = games_black_and_white(num_games, action_name.action_funcs.data(), 10);
         auto elapsed_in_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
                                  std::chrono::system_clock::now() - start)
                                  .count();
@@ -59,120 +166,3 @@ int main()
 
     return 0;
 }
-
-// let action_names = vec![
-//     // minimax, alpha beta
-//     ActionName {
-//         action_funcs: vec![
-//             mini_max::mini_max_arc(depth),
-//             random_action::random_action_arc(),
-//         ],
-//         name: format!("mini max depth {depth} vs. random"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             random_action::random_action_arc(),
-//             random_action::random_action_arc(),
-//         ],
-//         name: format!("random vs random"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             mini_max::mini_max_arc(depth),
-//             mini_max::mini_max_arc(depth),
-//         ],
-//         name: format!("mini max depth {depth} vs. mini max depth {depth}"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             mini_max::mini_max_arc(depth),
-//             alpha_beta::alpha_beta_arc(depth),
-//         ],
-//         name: format!(
-//             "mini max depth {depth} vs. alpha-beta depth {depth}"
-//         ),
-//     },
-//     // iterative deepening
-//     ActionName {
-//         action_funcs: vec![
-//             iterative_deepening::iterative_deepening_action_arc(1),
-//             iterative_deepening::iterative_deepening_action_arc(1),
-//         ],
-//         name: format!("iterative deepening 1ms vs. 1ms"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             iterative_deepening::iterative_deepening_action_arc(2),
-//             iterative_deepening::iterative_deepening_action_arc(1),
-//         ],
-//         name: format!("iterative deepening 2ms vs. 1ms"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             iterative_deepening::iterative_deepening_action_arc(5),
-//             iterative_deepening::iterative_deepening_action_arc(1),
-//         ],
-//         name: format!("iterative deepening 5ms vs. 1ms"),
-//     },
-//     // monte_carlo
-//     ActionName {
-//         action_funcs: vec![
-//             monte_carlo::monte_carlo_action_arc(num_playout),
-//             random_action::random_action_arc(),
-//         ],
-//         name: format!("monte_carlo num_playout {num_playout} vs random"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             monte_carlo::monte_carlo_action_arc(30),
-//             random_action::random_action_arc(),
-//         ],
-//         name: format!("monte_carlo num_playout 30 vs random"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             monte_carlo::monte_carlo_action_arc(num_playout),
-//             monte_carlo::monte_carlo_action_arc(30),
-//         ],
-//         name: format!("monte_carlo num_playout {num_playout} vs 30"),
-//     },
-//     // mcts
-//     ActionName {
-//         action_funcs: vec![
-//             mcts::mcts_action_arc(num_playout, MCTS_PARAMS),
-//             monte_carlo::monte_carlo_action_arc(num_playout),
-//         ],
-//         name: format!(
-//             "mcts {num_playout} vs monte_carlo num_playout {num_playout}"
-//         ),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             mcts::mcts_action_arc(num_playout, MCTS_PARAMS),
-//             mcts::mcts_action_arc(30, MCTS_PARAMS),
-//         ],
-//         name: format!("mcts num_playout {num_playout} vs 30"),
-//     },
-//     // thunder
-//     ActionName {
-//         action_funcs: vec![
-//             thunder::thunder_search_arc(num_playout),
-//             mcts::mcts_action_arc(num_playout, MCTS_PARAMS),
-//         ],
-//         name: format!("thunder vs. mcts num_playout {num_playout}"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             thunder::thunder_timebound_arc(1),
-//             mcts::mcts_timebound_arc(1, MCTS_PARAMS),
-//         ],
-//         name: format!("thunder vs. mcts 1ms"),
-//     },
-//     ActionName {
-//         action_funcs: vec![
-//             thunder::thunder_timebound_arc(1),
-//             iterative_deepening::iterative_deepening_action_arc(1),
-//         ],
-//         name: format!("thunder vs. iterative deepening 1ms"),
-//     },
-// ];

fe7e366922bb6721f0ede76a7ab79b5913286a94
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-29 20:44:06 +0900

    add test

--- cpp/test/ch05/monte_carlo_tree_search_test.cc
+++ cpp/test/ch05/monte_carlo_tree_search_test.cc
@@ -8,8 +8,9 @@ class MCTSTest : public ::testing::Test
 {
 protected:
     AlternateMazeState state;
+    Node node;
 
-    MCTSTest() : state(0) {}
+    MCTSTest() : state(0), node(state) {}
 };
 
 TEST_F(MCTSTest, MCTSAction)
@@ -23,5 +24,17 @@ TEST_F(MCTSTest, MCTSActionTimebound)
 {
     int actual = mcts_action_with_time_threshold(state, 10);
     int expected = 3;
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MCTSTest, Expand)
+{
+    int actual = node.child_nodes_.size();
+    int expected = 0;
+    EXPECT_EQ(actual, expected);
+
+    node.expand();
+    actual = node.child_nodes_.size();
+    expected = 4;
     EXPECT_EQ(actual, expected);
 }

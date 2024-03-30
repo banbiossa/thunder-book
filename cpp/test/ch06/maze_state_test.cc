85e44e582828366603e297476673a41b558f3bdb
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-30 17:04:07 +0900

    add state

--- cpp/test/ch06/maze_state_test.cc
+++ cpp/test/ch06/maze_state_test.cc
@@ -29,3 +29,49 @@ score(B): 0 y:2 x: 3
 )";
     EXPECT_EQ(actual, expected);
 }
+
+TEST_F(MazeStateTest, IsDone)
+{
+    EXPECT_EQ(state.is_done(), false);
+}
+
+TEST_F(MazeStateTest, Advance)
+{
+    state.advance(0, 0);
+    string actual = state.to_string();
+    string expected = R"(
+turn: 1
+score(A): 6 y:2 x: 2
+score(B): 8 y:2 x: 4
+
+3.3.3
+37373
+8.A.B
+34843
+44144
+)";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MazeStateTest, LegalActions)
+{
+    vector<int> actual = state.legal_actions(0);
+    vector<int> expected = {0, 1, 2, 3};
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MazeStateTest, WhiteScore)
+{
+    state.advance(0, 0);
+    double actual = state.white_score();
+    double expected = 0;
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MazeStateTest, Winner)
+{
+    state.advance(0, 0);
+    string actual = state.winner();
+    string expected = "B";
+    EXPECT_EQ(actual, expected);
+}

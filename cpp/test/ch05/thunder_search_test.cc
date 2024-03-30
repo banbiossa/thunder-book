79b6c5b876368bd97b31b3286447ac8024659689
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-30 12:23:57 +0900

    add test

--- cpp/test/ch05/thunder_search_test.cc
+++ cpp/test/ch05/thunder_search_test.cc
@@ -9,13 +9,33 @@ class ThunderSearchTest : public ::testing::Test
 {
 protected:
     AlternateMazeState state;
+    Node node;
 
-    ThunderSearchTest() : state(0) {}
+    ThunderSearchTest() : state(0), node(state) {}
 };
 
 TEST_F(ThunderSearchTest, ThunderSearchAction)
 {
     int actual = thunder_search_action(state, 10);
     int expected = 0;
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(ThunderSearchTest, ThunderSearchActionWithTime)
+{
+    int actual = thunder_search_action_with_timekeeper(state, 1);
+    int expected = 3;
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(ThunderSearchTest, Expand)
+{
+    int actual = node.child_nodes_.size();
+    int expected = 0;
+    EXPECT_EQ(actual, expected);
+
+    node.expand();
+    actual = node.child_nodes_.size();
+    expected = 4;
     EXPECT_EQ(actual, expected);
 }

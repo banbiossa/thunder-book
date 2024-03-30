925a5f4148a0a494ff994896c0461deff97e4f35
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-30 17:21:17 +0900

    add test

--- cpp/test/ch06/duct_test.cc
+++ cpp/test/ch06/duct_test.cc
@@ -11,8 +11,9 @@ class DuctTest : public ::testing::Test
 {
 protected:
     SimultaneousMazeState state;
+    Node node;
 
-    DuctTest() : state(0) {}
+    DuctTest() : state(0), node(state) {}
 };
 
 TEST_F(DuctTest, Duct)
@@ -22,4 +23,33 @@ TEST_F(DuctTest, Duct)
 
     actual = duct_action(state, 1, 3000);
     EXPECT_EQ(actual, 1);
+}
+
+TEST_F(DuctTest, Expand)
+{
+    int actual = node.child_nodeses_.size();
+    EXPECT_EQ(actual, 0);
+    node.expand();
+    actual = node.child_nodeses_.size();
+    EXPECT_EQ(actual, 4);
+}
+
+TEST_F(DuctTest, Explore)
+{
+    double actual = node.explore();
+    EXPECT_GE(actual, 0);
+}
+
+TEST_F(DuctTest, NextChildNode)
+{
+    node.expand();
+    auto &actual = node.next_child_node();
+    EXPECT_EQ(actual.n_, 0);
+}
+TEST_F(DuctTest, Action0)
+{
+    for (int i = 0; i < 1000; i++)
+        node.explore();
+    int actual = node.action0();
+    EXPECT_GE(actual, 0);
 }

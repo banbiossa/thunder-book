d99091dad54481c251f81d73566225bb531b8101
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-30 18:12:35 +0900

    add test

--- cpp/test/ch06/alternate_maze_state_test.cc
+++ cpp/test/ch06/alternate_maze_state_test.cc
@@ -7,23 +7,23 @@
 
 using namespace std;
 
-class MazeStateTest : public ::testing::Test
+class AlternateMazeStateTest : public ::testing::Test
 {
 protected:
     SimultaneousMazeState base_state;
     alternate::AlternateMazeState state;
 
-    MazeStateTest() : base_state(0), state(base_state, 0) {}
+    AlternateMazeStateTest() : base_state(0), state(base_state, 0) {}
 };
 
-TEST_F(MazeStateTest, LegalActions)
+TEST_F(AlternateMazeStateTest, LegalActions)
 {
-    // auto actual = state.legal_actions();
-    // auto expected = vector<int>{0, 1, 2, 3};
-    // EXPECT_EQ(actual, expected);
+    auto actual = state.legal_actions();
+    auto expected = vector<int>{0, 1, 2, 3};
+    EXPECT_EQ(actual, expected);
 }
 
-TEST_F(MazeStateTest, MakeState)
+TEST_F(AlternateMazeStateTest, MakeState)
 {
-    // EXPECT_EQ(state.is_done(), false);
+    EXPECT_EQ(state.is_done(), false);
 }

598424cddfadd57403438b67e5caf86837511748
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-31 18:33:55 +0900

    add test

--- cpp/test/ch08/maze_state_test.cc
+++ cpp/test/ch08/maze_state_test.cc
@@ -47,6 +47,41 @@ TEST_F(MazeStateTest, Advance)
     state.advance(6);
     string actual = state.to_string();
     string expected = R"(
+is_first: 0
+
+.......
+.......
+.......
+.......
+.......
+XOXOXOX
 )";
     EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MazeStateTest, TebanScore)
+{
+    double actual = state.teban_score();
+    double expected = 0.5;
+    EXPECT_EQ(actual, expected);
+
+    state.advance(0);
+    state.advance(1);
+    state.advance(0);
+    state.advance(1);
+    state.advance(0);
+    state.advance(1);
+
+    EXPECT_FALSE(state.is_done());
+
+    state.advance(0);
+    actual = state.teban_score();
+    expected = 0.0;
+    EXPECT_EQ(actual, expected);
+
+    actual = state.white_score();
+    expected = 1.0;
+    EXPECT_EQ(actual, expected);
+
+    EXPECT_TRUE(state.is_done());
 }

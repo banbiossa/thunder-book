b9a8da55da5dc02df420e514bd1ccc46a888f26b
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-31 18:32:57 +0900

    add tests

--- cpp/test/ch08/maze_state_test.cc
+++ cpp/test/ch08/maze_state_test.cc
@@ -17,6 +17,21 @@ TEST_F(MazeStateTest, ToString)
 {
     string actual = state.to_string();
     string expected = R"(
+is_first: 1
+
+.......
+.......
+.......
+.......
+.......
+.......
 )";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(MazeStateTest, LegalActions)
+{
+    vector<int> actual = state.legal_actions();
+    vector<int> expected = {0, 1, 2, 3, 4, 5, 6};
     EXPECT_EQ(actual, expected);
 }

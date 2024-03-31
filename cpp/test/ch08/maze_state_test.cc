39f431da5061a3674e0a6fd93d23eb89c6c5fe0c
shota shimizu <shota.shimizu@legalontech.jp>
2024-03-31 19:34:51 +0900

    try parametrize

--- cpp/test/ch08/maze_state_test.cc
+++ cpp/test/ch08/maze_state_test.cc
@@ -5,17 +5,54 @@
 
 using namespace std;
 
+class ConnectFourStateTest : public ::testing::TestWithParam<StateVersion>
+{
+protected:
+    std::unique_ptr<ConnectFourState> state;
+
+    void SetUp() override
+    {
+        state = get_state(GetParam());
+    }
+};
+
+TEST_P(ConnectFourStateTest, LegalActions)
+{
+    std::vector<int> actual = state->legal_actions();
+    std::vector<int> expected = {0, 1, 2, 3, 4, 5, 6};
+    EXPECT_EQ(actual, expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    AllStateVersions,
+    ConnectFourStateTest,
+    ::testing::Values(StateVersion::Normal, StateVersion::Bitset));
+
 class MazeStateTest : public ::testing::Test
 {
 protected:
-    ConnectFourStateNormal state;
+    std::unique_ptr<ConnectFourStateNormal> state;
 
-    MazeStateTest() : state() {}
+    void SetUp() override
+    {
+        state = std::make_unique<ConnectFourStateNormal>();
+    }
 };
 
-TEST_F(MazeStateTest, ToString)
+class BitsetTest : public ::testing::Test
 {
-    string actual = state.to_string();
+protected:
+    std::unique_ptr<ConnectFourStateBitset> state;
+
+    void SetUp() override
+    {
+        state = std::make_unique<ConnectFourStateBitset>();
+    }
+};
+
+TEST_P(ConnectFourStateTest, ToString)
+{
+    string actual = state->to_string();
     string expected = R"(
 is_first: 1
 
@@ -29,23 +66,17 @@ is_first: 1
     EXPECT_EQ(actual, expected);
 }
 
-TEST_F(MazeStateTest, LegalActions)
-{
-    vector<int> actual = state.legal_actions();
-    vector<int> expected = {0, 1, 2, 3, 4, 5, 6};
-    EXPECT_EQ(actual, expected);
-}
-
+// TEST_P(ConnectFourStateTest, Advance)
 TEST_F(MazeStateTest, Advance)
 {
-    state.advance(0);
-    state.advance(1);
-    state.advance(2);
-    state.advance(3);
-    state.advance(4);
-    state.advance(5);
-    state.advance(6);
-    string actual = state.to_string();
+    state->advance(0);
+    state->advance(1);
+    state->advance(2);
+    state->advance(3);
+    state->advance(4);
+    state->advance(5);
+    state->advance(6);
+    string actual = state->to_string();
     string expected = R"(
 is_first: 0
 
@@ -59,29 +90,30 @@ XOXOXOX
     EXPECT_EQ(actual, expected);
 }
 
+// TEST_P(ConnectFourStateTest, TebanScore)
 TEST_F(MazeStateTest, TebanScore)
 {
-    double actual = state.teban_score();
+    double actual = state->teban_score();
     double expected = 0.5;
     EXPECT_EQ(actual, expected);
 
-    state.advance(0);
-    state.advance(1);
-    state.advance(0);
-    state.advance(1);
-    state.advance(0);
-    state.advance(1);
+    state->advance(0);
+    state->advance(1);
+    state->advance(0);
+    state->advance(1);
+    state->advance(0);
+    state->advance(1);
 
-    EXPECT_FALSE(state.is_done());
+    EXPECT_FALSE(state->is_done());
 
-    state.advance(0);
-    actual = state.teban_score();
+    state->advance(0);
+    actual = state->teban_score();
     expected = 0.0;
     EXPECT_EQ(actual, expected);
 
-    actual = state.white_score();
+    actual = state->white_score();
     expected = 1.0;
     EXPECT_EQ(actual, expected);
 
-    EXPECT_TRUE(state.is_done());
+    EXPECT_TRUE(state->is_done());
 }

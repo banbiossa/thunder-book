305d8ca73c8e39ebb34cacbc021be698ecd06c14
shota shimizu <shota.shimizu@legalontech.jp>
2024-04-01 12:43:15 +0900

    add test

--- cpp/test/ch08/mcts_test.cc
+++ cpp/test/ch08/mcts_test.cc
@@ -35,3 +35,29 @@ TEST_P(MCTSTest, MCTSActionTimebound)
     int actual = mcts_action_timebound(state, 1);
     EXPECT_LE(actual, W);
 }
+
+TEST_P(MCTSTest, Expand)
+{
+    node->expand();
+    EXPECT_EQ(node->child_nodes_.size(), state->legal_actions().size());
+}
+
+TEST_P(MCTSTest, Playout)
+{
+    Playout playout = Playout(state->clone());
+    double actual = playout.playout();
+    EXPECT_LE(actual, 1.0);
+}
+
+TEST_P(MCTSTest, Evaluate)
+{
+    double actual = node->evaluate();
+    EXPECT_LE(actual, 1.0);
+}
+
+TEST_P(MCTSTest, NextChildNode)
+{
+    node->expand();
+    Node &next_child_node = node->next_child_node();
+    EXPECT_LE(next_child_node.evaluate(), 1.0);
+}
